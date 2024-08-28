26d48bbb95db3d5d6b32ebe2d7cc44bf7755297e
Krutivas Pradhan <72146433+krutivaspradhan@users.noreply.github.com>
2024-08-28 15:09:34 -0500

    Stars added

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -2,9 +2,21 @@
 using namespace std;
 
 int main(){
+
+	// Adding beautification
 	
+	for ( int i = 0; i < 20; i++)
+	{
+		cout << "*" 
+	} 
+	cout << endl;
 	cout << "Hello Git" << endl;
 	cout << "Hello Github" << endl;
 	cout << "Hello World!" << endl;
+	cout << endl;
+	for ( int i = 0; i < 20; i++)
+	{
+		cout << "*" 
+	}	
 	return 0;
 }
